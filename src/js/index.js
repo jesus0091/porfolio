682dd088198841672b381e934130273d7210bb55
JesusHernandez <jesushernandez120491@gmail.com>
2021-12-29 18:25:58 -0300

    ooooo

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,31 @@
-$(window).scroll(function() {
-  
-    // selectors
+$(window)
+  .scroll(function () {
     var $window = $(window),
-        $body = $('body'),
-        $panel = $('.panel');
-    
-    // Change 33% earlier than scroll position so colour is there when you arrive.
-    var scroll = $window.scrollTop() + ($window.height() / 3);
-   
+      $body = $("body"),
+      $panel = $(".panel");
+
+    var scroll = $window.scrollTop() + $window.height() / 3;
+
     $panel.each(function () {
       var $this = $(this);
-      
-      // if position is within range of this panel.
-      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-      // Remember we set the scroll to 33% earlier in scroll var.
-      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-            
-        // Remove all classes on body with color-
+
+      if (
+        $this.position().top <= scroll &&
+        $this.position().top + $this.height() > scroll
+      ) {
+
         $body.removeClass(function (index, css) {
-          return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+          return (css.match(/(^|\s)color-\S+/g) || []).join(" ");
         });
-         
-        // Add class of currently active div
-        $body.addClass('color-' + $(this).data('color'));
+
+        $body.addClass("color-" + $(this).data("color"));
       }
-    });    
-    
-  }).scroll();
+    });
+  })
+  .scroll();
 
-
-
-
-
-  const cursor = document.querySelector("#cursor"),
+const cursor = document.querySelector("#cursor"),
   anchor = document.querySelectorAll(".hover-this");
-
-//   custom cursor
 
 window.addEventListener("mousemove", (e) => {
   let x = e.pageX,
@@ -44,8 +34,6 @@ window.addEventListener("mousemove", (e) => {
   cursor.style.left = `${x}px`;
   cursor.style.top = `${y}px`;
 });
-
-// anchor tag hover effect
 
 anchor.forEach((anc) => {
   anc.addEventListener("mouseover", () => {
